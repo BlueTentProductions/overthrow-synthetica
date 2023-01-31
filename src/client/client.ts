fe3b2cc6ae510f333f778f10763a15d0e5c61b87
Anthony Leung <anthonyleung07@gmail.com>
2023-01-31 00:12:47 +0000

    fps controls framework done
    Made a fps controls framework
    
    Allows for fps camera movement, and also player movement. A fairly simple implementation, but a good framework to build upon later
    
    For now, to unpause the game, just click on the screen, and either esc or tab out to pause the game. Game will freeze when paused, but will resume movement when unpaused (done intentionally because that's how pausing works. see isPaused). Made use of cursor locking.

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,46 +1,74 @@
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { Controls } from './fps-controls'
 
-const scene = new THREE.Scene()
+var scene: THREE.Scene
+var camera: THREE.PerspectiveCamera
+var renderer: THREE.WebGLRenderer
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-camera.position.z = 2
+var controls: Controls
 
-const renderer = new THREE.WebGLRenderer()
-renderer.setSize(window.innerWidth, window.innerHeight)
-document.body.appendChild(renderer.domElement)
+var floor: THREE.Mesh
 
-const controls = new OrbitControls(camera, renderer.domElement)
+var prevTime: number = performance.now()
 
-const geometry = new THREE.BoxGeometry()
-const material = new THREE.MeshBasicMaterial({
-    color: 0x00ff00,
-    wireframe: true,
-})
+function init() {
+    scene = new THREE.Scene()
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+    camera.position.y = 20
+    camera.position.z = 2
 
-const cube = new THREE.Mesh(geometry, material)
-scene.add(cube)
-
-window.addEventListener('resize', onWindowResize, false)
-function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight
-    camera.updateProjectionMatrix()
+    renderer = new THREE.WebGLRenderer()
     renderer.setSize(window.innerWidth, window.innerHeight)
-    render()
+    document.body.appendChild(renderer.domElement)
+    
+    controls = new Controls(camera)
+    
+    const floorGeo: THREE.PlaneGeometry = new THREE.PlaneGeometry(1000, 1000, 64, 64)
+    floorGeo.rotateX(- Math.PI / 2)
+    const floorMat: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({
+        color: 0x00ff00,
+        wireframe: true,
+    })
+
+    floor = new THREE.Mesh(floorGeo, floorMat)
+    scene.add(floor)
+    
+    window.addEventListener('resize', () => {
+        camera.aspect = window.innerWidth / window.innerHeight
+        camera.updateProjectionMatrix()
+        renderer.setSize(window.innerWidth, window.innerHeight)
+        render()
+    })
+    
+    document.body.addEventListener('click', () => {
+        // only activate controls when game is unpaused
+        controls.activate()
+    })
 }
 
 function animate() {
     requestAnimationFrame(animate)
 
-    cube.rotation.x += 0.01
-    cube.rotation.y += 0.01
+    const time: number = performance.now()
+    const delta: number = (time - prevTime) / 1000
 
-    controls.update()
+    prevTime = time
+
+    if (!isPaused()) {
+        controls.update(delta)
+    }
 
     render()
+}
+
+function isPaused() {
+    if (document.pointerLockElement === document.body) return false
+    else return true
 }
 
 function render() {
     renderer.render(scene, camera)
 }
+
+init()
 animate()
---
+++ src/client/fps-controls.ts
@@ -0,0 +1,132 @@
+import * as THREE from 'three'
+import { Vector3 } from 'three';
+
+export class Controls {
+    // look
+    activated: boolean = false
+    getCamera: Function
+
+    // movement
+    moveForward: boolean = false
+    moveBackward: boolean = false
+    moveRight: boolean = false
+    moveLeft: boolean = false
+
+    velocity: THREE.Vector3 = new THREE.Vector3(0, 0, 0)
+
+    decceleration: THREE.Vector3 = new THREE.Vector3(-10, -16, -10)
+    acceleration: THREE.Vector3 = new THREE.Vector3(500, 0, 500)
+
+    // variables for customisation
+    sensitivity: number = 1.0
+
+    constructor(camera: THREE.PerspectiveCamera) {
+        this.getCamera = () => {
+            return camera
+        }
+        
+        document.body.addEventListener('mousemove', (event: MouseEvent) => {
+            if (!this.activated) return
+
+            const euler: THREE.Euler = new THREE.Euler(0, 0, 0, 'YXZ') // ensure rotation is done on Y axis first
+
+            euler.setFromQuaternion(camera.quaternion)
+
+            euler.y -= event.movementX * 0.001 * this.sensitivity
+            euler.x -= event.movementY * 0.001 * this.sensitivity
+
+            // ensure that the player can only move the camera for max 180 degrees up and down
+            euler.x = THREE.MathUtils.clamp(euler.x, -Math.PI / 2, Math.PI / 2)
+
+            camera.quaternion.setFromEuler(euler)
+        })
+        
+        document.addEventListener('pointerlockchange', (event: Event) => {
+            // checks if pointerLockElement is the document.body or null, then change the activation correspondingly
+            if (document.pointerLockElement === document.body) this.activated = true
+            else this.activated = false
+        })
+
+        document.addEventListener('keydown', (event: KeyboardEvent) => {
+            switch (event.code) {
+                case "KeyW":
+                    this.moveForward = true;
+                    break;
+                case "KeyS":
+                    this.moveBackward = true;
+                    break;
+                case "KeyD":
+                    this.moveRight = true;
+                    break;
+                case "KeyA":
+                    this.moveLeft = true;
+                    break;
+            }
+        });
+        document.addEventListener('keyup', (event: KeyboardEvent) => {
+            switch (event.code) {
+                case "KeyW":
+                    this.moveForward = false;
+                    break;
+                case "KeyS":
+                    this.moveBackward = false;
+                    break;
+                case "KeyD":
+                    this.moveRight = false;
+                    break;
+                case "KeyA":
+                    this.moveLeft = false;
+                    break;
+            }
+        });
+    }
+
+    activate() {
+        // lock cursor to allow look and movement
+        document.body.requestPointerLock()
+    }
+
+    update(delta: number) {
+        this.deccelerate(delta)
+        this.accelerate(delta)
+        this.movement(delta)
+    }
+
+    deccelerate(delta: number) {
+        const decceleration: THREE.Vector3 = new THREE.Vector3(0, 0, 0)
+
+        // decceleration must be a fading gradient of the previous values, also making sure player stops when velocity is 0
+        decceleration.x = this.velocity.x * this.decceleration.x
+        decceleration.z = this.velocity.z * this.decceleration.z
+
+        decceleration.multiplyScalar(delta)
+
+        this.velocity.add(decceleration)
+    }
+
+    accelerate(delta: number) {
+        const acceleration: THREE.Vector3 = new THREE.Vector3(0, 0, 0)
+
+        // acceleration determined by direction chosen by player, which can only be one out of the two polar opposites
+        acceleration.x = (Number(this.moveRight) - Number(this.moveLeft)) * this.acceleration.x
+        acceleration.z = (Number(this.moveForward) - Number(this.moveBackward)) * this.acceleration.z
+
+        acceleration.multiplyScalar(delta)
+
+        this.velocity.add(acceleration)
+    }
+
+    movement(delta: number) {
+        const forward: THREE.Vector3 = (new THREE.Vector3(0, 0, -1)).applyQuaternion(this.getCamera().quaternion)
+        forward.y = 0
+        forward.normalize()
+        forward.multiplyScalar(this.velocity.z * delta)
+        
+        const right: THREE.Vector3 = (new THREE.Vector3(1, 0, 0)).applyQuaternion(this.getCamera().quaternion)
+        right.y = 0
+        right.normalize()
+        right.multiplyScalar(this.velocity.x * delta)
+        
+        this.getCamera().position.add(forward).add(right);
+    }
+}
