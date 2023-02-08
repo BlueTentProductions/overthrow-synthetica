60a45f7c73491531672637c7a7fe59edea24adf3
codethulu <brendanbell2001@hotmail.com>
2023-02-08 00:53:24 +0000

    updated workflow

--- src/client/webpack.common.js
+++ src/client/webpack.common.js
@@ -20,6 +20,6 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, '../../dist/client'),
-        publicPath: '/dist/client/',
+        publicPath: '',
     }
 };
