df27b21741a0457d2dc6242a6dc69608bc9b48b8
codethulu <brendanbell2001@hotmail.com>
2023-02-08 13:27:58 +0000

    retro mode off default

--- src/client/js/Game.ts
+++ src/client/js/Game.ts
@@ -15,9 +15,8 @@ import { Pedestrian, Officer } from './Npc';
 import Entity from './Entity';
 
 let loader = new GLTFLoader();
-
 let RENDER_DISTANCE = 100;
-let RETRO_MODE = true;
+let RETRO_MODE = false;
 
 export default class Game {
     _scene: THREE.Scene;
